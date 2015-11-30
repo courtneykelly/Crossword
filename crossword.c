e16b9a66d5a912c005d3ffc181c2b81ee1a8c2eb
Courtney Katherine Kelly <ckelly19@student00.cse.nd.edu>
2015-11-30 14:34:50 -0500

    Initialized user input

--- crossword.c
+++ crossword.c
@@ -3,9 +3,33 @@
 // Katie Schermerhorn and Courtney Kelly
 
 #include <stdio.h>
+#include <string.h>
 
-
+// Functions
 
 int main() {
 
+// Get User Input
+	char key[2] = ".";
+	char words[20][16];
+	int i=0, loop=1;
+
+	printf("Anagram Crossword Puzzle Generator\n----------------------------------\n");
+	printf("Enter a list of words:\n");
+	while (loop) {
+		fgets(words[i], 16, stdin);
+		if (strcasestr(words[i], key)) {
+			loop=0;
+		}
+		i++;
+	}
+
+// Initialize Board
+	
+
+// Generate Puzzle
+	
+
+// Display Hints 
+	
 }
