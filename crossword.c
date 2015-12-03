8462315348f1286d48ca8bb93f2b2b71fcd8e67b
Courtney Katherine Kelly <ckelly19@student00.cse.nd.edu>
2015-12-02 20:45:30 -0500

    Implemented "Can Place Vertical" Function

--- crossword.c
+++ crossword.c
@@ -14,6 +14,7 @@ int compare(char * a, char * b);
 void initialPlace(char [][16], char [][15]);
 int placeWord(char [], char [][15]);
 int can_place_word_horiz(char [], char [][15], int, int, int, int);
+int can_place_word_vert(char [], char [][15], int, int, int, int);
 
 int main() {
 
@@ -44,11 +45,13 @@ int main() {
 
 	int n;
 
-	for(n=1; n<i; n++) {
-	  if(placeWord(words[n], board)==0) {
-	    //print word on board
-	    printf("it failed!");
-	  }
+	for(n=1; n<(i-1); n++) {
+	  	if(placeWord(words[n], board)) {
+	   		printf("SUCCESS!");
+		}
+		else {
+			printf("FAILURE!");
+		}
 	}
 
 
@@ -114,10 +117,13 @@ void initialPlace(char words[21][16], char board[15][15]) {
        			for(col=0; col<15; col++){
 	 			if (board[row][col]==word[i]) {
 					printf("%c", word[i]);
-	   				if(can_place_word_horiz(word, board, row, col, i, length)){
+	   				if (can_place_word_horiz(word, board, row, col, i, length)) {
 	   					return place_success;
 	   				}
-	 			}
+					else if (can_place_word_vert(word, board, row, col, i, length)) {
+	 					return place_success;
+					}
+				}
        			}
      		}
    	}
@@ -149,9 +155,29 @@ int can_place_word_horiz(char * word, char board[15][15], int row, int col, int 
 		}
 	return place_success;
 }
-	// vertical
-		// second half of the word 
-		// first half of the word
-		// left of word col-l
-		// right of word col+1
+int can_place_word_vert(char * word, char board[15][15], int row, int col, int i, int length) {
 
+	char key= '.';
+        int n,k;
+	
+	// Checking against height of the board
+		if ((length-i) > (15-row) || ((row-i+1)< 0)) {
+                        return place_failure;
+                }
+	// Checking immediently to the left through immedietly to the right of potenital place for word
+		for (n=(col-1); n<=(col+1); n++) {
+                        for (k=(row-i+1); k<=(row+(length-i)); k++) {
+                                if (k==row) {
+                                        // disregards column bc letters can be there
+                                }
+                                else if (board[k][n] != key) {
+					return place_failure;
+				}
+			}
+		}
+	// Checking the enpoints of the word
+		if ((board[row-i][col] != key) || (board[row+(length-i+1)][col] != key)) {
+                        return place_failure;
+                }
+        return place_success;
+}
